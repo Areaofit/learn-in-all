e788cdebcfc8909d34487121ec884fbb168dd75f
Areaofit <areaofit@qq.com>
2018-12-25 21:05:21 +0800

    commit

---
+++ netty-learn/src/main/java/com/areaofit/startup/bio/TimeServer.java
@@ -0,0 +1,37 @@
+package com.areaofit.startup.bio;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TimeServer {
+
+    // 端口号
+    public static int port = 8080;
+
+    public static void main(String[] args) throws Exception {
+        if (args != null && args.length > 0 && !"".equals(args[0]))
+        {
+            int p = Integer.parseInt(args[0]);
+            if (p > 0)
+            {
+                port = p;
+            }
+        }
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("BIO Time server is startup, port is " + port);
+        try {
+            Socket socket;
+            while (true) {
+                socket = serverSocket.accept();
+                new Thread(new TimeServerHandler(socket)).start();
+            }
+        } finally {
+            if (serverSocket != null)
+            {
+                System.out.println("BIO Time server is close.");
+                serverSocket.close();
+            }
+        }
+
+    }
+}
